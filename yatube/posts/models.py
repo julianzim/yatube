a7bacff00897fda1a0a7bd3a678efe9c7667a122
JulianZim <ziminjulian@gmail.com>
2024-03-09 18:43:39 +0300

    registered Post model

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Post(models.Model()):
+class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('date_published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
